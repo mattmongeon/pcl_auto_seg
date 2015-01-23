1c691e942b65af1e2b237fa57c11763169a8289d
Matt Mongeon <matthewmongeon2015@u.northwestern.edu>
2015-01-23 14:14:09 -0600

    Initial commit.  For convenience for saving new point clouds to disk for offline development/debugging.

---
+++ src/save_pt_cloud.cpp
@@ -0,0 +1,34 @@
+#include <ros/ros.h>
+
+#include <sensor_msgs/PointCloud2.h>
+
+#include <pcl_ros/point_cloud.h>
+
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+
+void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
+{
+    // We are simply going to save a point cloud to file for now.
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::fromROSMsg(*input, *cloud);
+
+    pcl::io::savePCDFileASCII("/home/mongeon/point_cloud.pcd", *cloud);
+
+    exit(0);
+}
+
+int main(int argc, char** argv)
+{
+    // Initialize ROS
+    ros::init(argc, argv, "pcl_auto_seg");
+    ros::NodeHandle nh;
+
+    // Subscriber for taking in the input point cloud.
+    ros::Subscriber sub = nh.subscribe("camera/depth_registered/points", 1, cloud_cb);
+
+    ros::spin();
+
+    return 0;
+}
+
